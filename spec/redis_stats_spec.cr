c726b9023e32b927674ca0f3ee578ee1af344131
SShekotihin <sshekotihindev@gmail.com>
2022-01-12 14:58:04 +0300

    implementation

--- spec/redis_stats_spec.cr
+++ spec/redis_stats_spec.cr
@@ -1,9 +1,34 @@
 require "./spec_helper"
 
 describe RedisStats do
-  # TODO: Write tests
+  it "configurable" do
+    client = Redis::PooledClient.new(
+      host: "localhost",
+      port: 6379,
+      database: 0,
+      pool_size: 10
+    )
 
-  it "works" do
-    false.should eq(true)
+    RedisStats.configure do |config|
+      config.redis = client
+      config.ttl = 10.minutes
+      config.prefix = "redis_stats"
+    end
+
+    RedisStats.settings.redis.should eq client
+    RedisStats.settings.ttl.should eq 10.minutes
+    RedisStats.settings.prefix.should eq "redis_stats"
+  end
+
+  describe "#add" do
+    # TODO: write specs
+  end
+
+  describe "#stats" do
+    # TODO: write specs
+  end
+
+  describe "#del_expired_stats" do
+    # TODO: write specs
   end
 end
--- src/redis_stats.cr
+++ src/redis_stats.cr
@@ -1,6 +1,108 @@
-# TODO: Write documentation for `RedisStats`
+require "habitat"
+require "redis"
+
+# :nodoc:
 module RedisStats
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  Habitat.create do
+    # redis connection
+    setting redis : Redis | Redis::PooledClient
+    # cache-key prefix
+    setting prefix : String = "stats"
+    # stats ttl
+    setting ttl : Time::Span = 1.day
+  end
+
+  # Adds metric to Redis.
+  def self.add(key : String, duration : Float64)
+    parsed_values = values(key: key)
+    parsed_values << NamedTuple.new(created_at: Time.local, duration: duration)
+
+    settings.redis.hset(settings.prefix, key, parsed_values.to_json)
+  end
+
+  # Generates general statistics for all keys hashset.
+  #
+  # Returns array of hashes with stats.
+  def self.stats : Array(Hash(String, Hash(String, String)))
+    keys.compact_map do |key|
+      data = _stats(key)
+
+      { key => data } if data
+    end
+  end
+
+  # Removes all expired statistics.
+  def self.del_expired_stats
+    keys.each { |key| _del_expired_stats(key: key) }
+  end
+
+  # Generates statistics for key in hashset.
+  #
+  # Returns hash with stats.
+  private def self._stats(key : String) : Hash(String, String)
+    values = values(key: key)
+    return({} of String => String) unless values
+
+    # 5m 30m 1day
+    five = [] of Float64
+    thirty = [] of Float64
+    day = [] of Float64
+
+    values.each do |value|
+      five << value["duration"] if value["created_at"] > 5.minutes.ago
+      thirty << value["duration"] if value["created_at"] > 30.minutes.ago
+      day << value["duration"] if value["created_at"] > 1.day.ago
+    end
+
+    {
+      "5m" => !five.empty? ? "#{(five.sum / five.size).round(2)}ms" : "",
+      "30m" => !thirty.empty? ? "#{(thirty.sum / thirty.size).round(2)}ms" : "",
+      "1d" => !day.empty? ? "#{(day.sum / day.size).round(2)}ms" : "",
+      "max" => !day.empty? ? "#{day.max.round(2)}ms" : "",
+      "min" => !day.empty? ? "#{day.min.round(2)}ms" : "",
+      "count" => day.size.to_s
+    }
+  end
+
+  # Removes all expired statistics for a specific key.
+  private def self._del_expired_stats(key : String)
+    parsed_values = values(key: key)
+    parsed_values = parsed_values.select { |value| value["created_at"] > settings.ttl.ago }
+
+    # пишем обновленную статистику
+    settings.redis.hset(settings.prefix, key, parsed_values.to_json)
+  end
+
+  # Retrieves all values from Redis for a specific key.
+  #
+  # Returns array of named tuples with stats.
+  private def self.values(key : String) : Array(NamedTuple(created_at: Time, duration: Float64))
+    values = settings.redis.hget(settings.prefix, key)
+
+    if values
+      Array(NamedTuple(created_at: Time, duration: Float64)).from_json(values)
+    else
+      [] of NamedTuple(created_at: Time, duration: Float64)
+    end
+  end
+
+  # Form array with all presented stats keys in Redis.
+  #
+  # Return array of string keys.я
+  private def self.keys
+    index = 0
+
+    settings.redis.hgetall(settings.prefix).compact_map do |v|
+      if index % 2 != 0
+        index += 1
+        next
+      end
+
+      index += 1
+
+      v.to_s
+    end
+  end
 end
